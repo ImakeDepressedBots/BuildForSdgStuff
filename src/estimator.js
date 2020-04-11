831a3df2b1a99edcafea4cdd918f88543cf67173
Jayson Amati <jaysonamati@gmail.com>
2020-04-11 22:34:27 +0300

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -30,8 +30,8 @@ const covid19ImpactEstimator = (data) => {
   const casesForICUByRequestedTimeSevereImpact = Math.floor(0.05 * infectionsByRequestedTimeSevereImpact);
   const casesForVentilatorsByRequestedTimeImpact = Math.floor(0.02 * infectionsByRequestedTimeImpact);
   const casesForVentilatorsByRequestedTimeSevereImpact = Math.floor(0.02 * infectionsByRequestedTimeSevereImpact);
-  const dollarsInFlightImpact = (infectionsByRequestedTimeImpact * incomePopulation) * dailyIncome;
-  const dollarsInFlightSevere = (infectionsByRequestedTimeSevereImpact * incomePopulation) * dailyIncome;
+  const dollarsInFlightImpact = (infectionsByRequestedTimeImpact * incomePopulation);
+  const dollarsInFlightSevere = (infectionsByRequestedTimeSevereImpact * incomePopulation);
 
 
   return {
